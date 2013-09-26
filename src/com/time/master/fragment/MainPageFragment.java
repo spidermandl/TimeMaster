b474003b2f102ebd964a5041d4cbe66b803e42bd
mingmingzhizhong <1553278345@qq.com>
2013-09-26 11:34:51 +0800

    helloa

--- src/com/time/master/fragment/MainPageFragment.java
+++ src/com/time/master/fragment/MainPageFragment.java
@@ -2,12 +2,14 @@ package com.time.master.fragment;
 
 
 import com.time.master.R;
+import com.time.master.fragment.*; 
 import com.time.master.activity.FrameActivity;
 import com.time.master.activity.MainActivity;
 import com.time.master.view.BasicTextView;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,15 +19,14 @@ import android.widget.TextView;
 
 
 public class MainPageFragment extends Fragment implements OnClickListener{
+	
+	
 	@Override
-	
-	
-	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View layout = inflater.inflate(R.layout.main_page, container, false);
-		BasicTextView add=(BasicTextView)layout.findViewById(R.id.main_page_add);
+		View layout = inflater.inflate(R.layout.date_one, container, false);
+		BasicTextView add=(BasicTextView)layout.findViewById(R.id.main_plus);
 		add.setOnClickListener(this);
 		
 		return layout;
@@ -35,10 +36,13 @@ public class MainPageFragment extends Fragment implements OnClickListener{
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.main_page_add:
+		case R.id.main_plus:
 			FrameActivity activity=(FrameActivity)getActivity();
-			activity.showNext(new Page4Fragment(), R.id.date_fragment);
+			activity.showNext(new DateTwoFragment(), R.id.date_fragment);
 			break;
+			case R.id.main_live:
+				FragmentActivity activity2=(FragmentActivity)getActivity();
+				activity2.showNext()
 
 		default:
 			break;
