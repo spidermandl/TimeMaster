a00dcd210c5fcffd050576a867a2014c171cdca4
unknown <ZYJ@.(none)>
2013-10-09 15:35:24 +0800

    1

--- src/com/time/master/dialog/WorldTimeDialogFragment.java
+++ src/com/time/master/dialog/WorldTimeDialogFragment.java
@@ -44,7 +44,7 @@ public  class WorldTimeDialogFragment extends android.support.v4.app.DialogFragm
         window.clearFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND | WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         
         View layout=inflater.inflate(R.layout.world_time_layout, container, false);
-        View context=layout.findViewById(R.id.worldtime);
+        View basicview=layout.findViewById(R.id.worldtime);
 		return layout;
 	}
 
